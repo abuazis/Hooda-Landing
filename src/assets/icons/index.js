10c87185c86014c70a186ed2718ed6997bb7546a
rezak400 <k400ezza@gmail.com>
2021-02-05 21:13:03 +0700

    add icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -5,6 +5,10 @@ import IcFlash from "./ic_flash.png";
 import IcFood from "./ic_food.png";
 import IcToday from "./ic_today.png";
 import IcTomorrow from "./ic_tomorrow.png";
+import FacebookIcon from "./facebook_icon";
+import InstagramIcon from './instagram_icon'
+import TwitterIcon from './twitter_icon'
+import YoutubeIcon from './youtube_icon'
 
 export {
   HoodaLogo,
@@ -14,4 +18,8 @@ export {
   IcFood,
   IcToday,
   IcTomorrow,
+  FacebookIcon,
+  InstagramIcon,
+  YoutubeIcon,
+  TwitterIcon
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HoodaLogo, FooterImage, PlayStore, AppStore } from '../../assets'
+import { HoodaLogo, FooterImage, PlayStore, AppStore, FacebookIcon, InstagramIcon, TwitterIcon, YoutubeIcon } from '../../assets'
 
 const Footer = () => {
     return (
@@ -52,27 +52,16 @@ const Footer = () => {
                             <p>Ikuti Kami</p>
                             <span class="inline-flex  mt-2 justify-center sm:justify-start">
                                 <a href="/#" class="">
-                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                </svg>
+                                    <FacebookIcon />
                                 </a>
                                 <a href="/#" class="ml-3 ">
-                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                                </svg>
+                                    <InstagramIcon />
                                 </a>
                                 <a href="/#" class="ml-3 ">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                                </svg>
+                                    <TwitterIcon />
                                 </a>
                                 <a href="/#" class="ml-2 mb-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-youtube" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                            <rect x="3" y="2" width="18" height="14" rx="4"></rect>
-                                        <path d="M10 6l5 3l-5 3z"></path>
-                                    </svg>
+                                    <YoutubeIcon />
                                 
                                 </a>
                     </span>
@@ -87,7 +76,7 @@ const Footer = () => {
                    
                     <div className="mx-auto text-center ">
                         <p className="flex justify-center md:justify-start">Unduh</p>
-                        <div className="flex mt-2">
+                        <div className="flex mt-3">
                             <img src={PlayStore} width="150" alt=""/>
                             <img className="ml-3" src={AppStore} width="150" alt=""/>
                         </div>
@@ -95,7 +84,7 @@ const Footer = () => {
                 </section>
 
                 {/* //! Copuright paling bawah  */}
-                <div class="bg-primary">
+                <div class="bg-primary mt-2">
                     <div class="container mx-auto py-4 px-5 ">
                     <p class=" text-xs text-center">© CopyrightbyHooda
                     </p>
