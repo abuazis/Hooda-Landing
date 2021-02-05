217d09988a56be757bcf788e780467e0d500ced7
rezak400 <k400ezza@gmail.com>
2021-02-05 16:34:56 +0700

    add top sidebar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,8 +6,37 @@ import React, { useRef, useState } from 'react'
 import { HoodaLogo, NavBarIcon } from '../../assets'
 import LanguageIcon from '@material-ui/icons/Language';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Drawer, Button, Grow, Paper, Popper, ClickAwayListener, MenuList, MenuItem } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+import { Drawer, Button, Grow, Paper, Popper, ClickAwayListener, MenuList, MenuItem, makeStyles, AccordionSummary, Typography, AccordionDetails, withStyles } from '@material-ui/core';
 
+//!style sidebar
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      flexBasis: '33.33%',
+      flexShrink: 0,
+    },
+    secondaryHeading: {
+      fontSize: theme.typography.pxToRem(15),
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+  const Accordion = withStyles({
+    root: {
+      backgroundColor: "#F15D2A",
+      color: "white",
+      listStyle : "none"
+    },
+    expanded: {},
+  })(MuiAccordion);
+  
 const NavBar = () => {
     const [sideBar, setSideBar] = useState(false)
     const [menu, setMenu] = useState({
@@ -34,11 +63,60 @@ const NavBar = () => {
 
     }
 
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     const sidebar_content = (
-        <div className="bg-primary text-white h-44 ">
-            <div className="container mx-auto">
-                <h1>Haloo dari drawerr</h1>
-            </div>
+        <div className="font-primary bg-primary font-semibold">
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} TransitionProps={{ unmountOnExit: true }}> 
+                <p className="py-4 px-3 text-sm">Tentang Kami</p>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2bh-content"
+                id="panel2bh-header"
+                >
+                <p className="text-sm">
+                    Layanan Kami
+                </p>
+               
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div className="text-sm">
+                        <p>Layanan 1</p>
+                        <p className="mt-2">Layanan 2</p>
+                        <p className="mt-2">Layanan 3</p>
+                    </div>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel3bh-content"
+                id="panel3bh-header"
+                >
+                <p className="text-sm">
+                    Gabung Mitra Kami
+                </p>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div className="text-sm">
+                        <p>Mitra 1</p>
+                        <p className="mt-2">Mitra 2</p>
+                        <p className="mt-2">Mitra 3</p>
+                    </div>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                    <p className="py-4 px-3 text-sm">
+                        Pusat Bantuan
+                    </p>
+            </Accordion>
         </div>
     )
     
@@ -142,7 +220,7 @@ const NavBar = () => {
 
                     <a href="/#">Pusat Bantuan</a>
                 </nav>
-                <div className="ml-auto  flex-wrap mr-5">
+                <div className="ml-auto  flex-wrap">
                     <Button ref={menu3Ref} onClick={() => handleMenu("menu3",true)} >
                         {/* //! isi  */}
                         <LanguageIcon className="text-white text-xs" fontSize="small"/>
@@ -163,7 +241,7 @@ const NavBar = () => {
                             <Paper>
                                 <ClickAwayListener onClickAway={() => handleMenu("menu3",false)}>
                                 <MenuList  id="menu-list-grow" >
-                                        {["Bahasa1","Bahasa2","Bahasa3"].map(item => {
+                                        {["Bhs","Bhs","Bhs"].map(item => {
                                             return <MenuItem>
                                                 <p className="font-primary text-xs">
                                                     {item}
